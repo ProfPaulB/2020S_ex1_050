3831215a1ba5f1a383a76787773d389ddda157e4
Daljeet Singh <sin10510@sheridancollege.ca>
2020-05-28 10:45:53 -0400

    Updated the program for user input.

--- src/pickacard/CardTrick.java
+++ src/pickacard/CardTrick.java
@@ -1,26 +1,50 @@
 package pickacard;
+import java.util.Scanner;
 
 /**
- * A class that fills a magic hand of 7 cards with random Card Objects and then asks the user to pick a card and
- * searches the array of cards for the match to the user's card. To be used as starting code in ICE 1
+ * A class that fills a magic hand of 7 cards with random Card Objects and then
+ * asks the user to pick a card and searches the array of cards for the match to
+ * the user's card.
  *
  * @author Daljeet Singh 28 May 2020
+ * @version 1.0
  */
 public class CardTrick {
 
     public static void main(String[] args) {
-        
-        Card[] magicHand = new Card[7];
 
+        Scanner n = new Scanner(System.in);
+        Card[] magicHand = new Card[7];
+        int[] tempValue = new int[7];                  // temporary variable for holding the values(int) of different cards.
+        String[] tempSuit = new String[7];             // temporary variable for holding the suit(String) of different cards.
+        int check = 0;                                 // int variable for checking the match.
         for (int i = 0; i < magicHand.length; i++) {
             Card c = new Card();
-            //c.setValue(insert call to random number generator here)
-            //c.setSuit(Card.SUITS[insert call to random number between 0-3 here])
+            c.setValue((int) (Math.random() * 13));    
+            c.setSuit(Card.SUITS[(int) (Math.random() * 3)]); 
+            tempValue[i] = c.getValue();               
+            tempSuit[i] = c.getSuit();
         }
 
-        //insert code to ask the user for Card value and suit, create their card
-        // and search magicHand here
-        //Then report the result here
-    }
+        // Asking user for input of card.
+        System.out.println("Enter the suit of card  (Hearts, Diamonds, Spades, Clubs)");
+        String suit = n.nextLine();
+        System.out.println("Enter the value of card ( Ace = 1, 2-10, Jack =11, Queen =12, King = 13)");
+        int value = n.nextInt();               
+        
+        //Searching the magic hand of 7 cards for user's card.
+        for (int i = 0; i < magicHand.length; i++) {     
+            if ((value == tempValue[i]) && (suit.equals(tempSuit[i]))) {
+                check = 1;
+            }
+        }
+        
+        //Displaying the result.
+        if (check == 1) {
+            System.out.printf("Congrats, your %d of %s card is found in magic hand of 7 cards.\n", value, suit);
+        } else {
+            System.out.printf("Sorry, your %d of %s card could not be found in magic hand of 7 cards\n", value, suit);
+        }
 
+    }
 }
