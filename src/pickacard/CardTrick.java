474af82b7ed8b640f41b54da238b1cce95d10c93
bains <bains@192.168.0.32>
2020-05-28 02:12:34 -0400

    First Commit

--- src/pickacard/CardTrick.java
+++ src/pickacard/CardTrick.java
@@ -7,21 +7,62 @@ package pickacard;
  * @author dancye
  * @author Paul Bonenfant
  */
-public class CardTrick {
+import javax.swing.*;
+  public class CardTrick { // class started
+    /**
+     * This is the main method
+     * @param args
+     */
+    public static void main(String[] args) { // method started
 
-    public static void main(String[] args) {
-        
-        Card[] magicHand = new Card[7];
+        String str1 = JOptionPane.showInputDialog("Enter the card value between 1 and 13");// User will input value of card
+        int a = Integer.parseInt(str1);
 
-        for (int i = 0; i < magicHand.length; i++) {
+        String str2 = JOptionPane.showInputDialog(" Choose one of the suit :(Hearts, Diamonds, Spades, Clubs) "); // User will input suit
+
+
+    Card[] magicHand = new Card[7];
+
+    String[] suit = new String[7];
+
+    int [] value = new int[7];
+
+
+     for (int i = 0; i < magicHand.length; i++)
+     {
             Card c = new Card();
-            //c.setValue(insert call to random number generator here)
-            //c.setSuit(Card.SUITS[insert call to random number between 0-3 here])
+
+
+            c.setValue((int)(1 + Math.random()*12));
+            c.setSuit(Card.SUITS[(int)Math.random()*4]);
+
+            suit[i] = c.getSuit();
+            value[i] = c.getValue();
+
+
         }
 
-        //insert code to ask the user for Card value and suit, create their card
-        // and search magicHand here
-        //Then report the result here
-    }
+
+   for(int i = 0 ; i< magicHand.length ; i++)
+   {
+   if(value[i]== a && suit[i] == str2)
+			 System.out.println("You got a magicHand");
+
+			 else
+			 System.out.println("You didn't got a magicHand");
 
 }
+   for(int i = 0 ; i <suit.length ; i++)
+   {
+       System.out.println(suit[i] + " : " + value[i]);
+   }
+
+
+
+
+    } // method closed
+
+} // class closed
+
+
+
