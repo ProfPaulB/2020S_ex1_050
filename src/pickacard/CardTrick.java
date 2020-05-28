192beca4d821fbdbdcc82f61cc1a89335c453e08
1GURT <1GURT@DESKTOP-E8J0T1L>
2020-05-28 00:52:16 -0400

    Updated the code.

--- src/pickacard/CardTrick.java
+++ src/pickacard/CardTrick.java
@@ -1,27 +1,54 @@
 package pickacard;
-
+import java.util.*;     //importing util package
 /**
  * A class that fills a magic hand of 7 cards with random Card Objects and then asks the user to pick a card and
  * searches the array of cards for the match to the user's card. To be used as starting code in ICE 1
  *
- * @author dancye
- * @author Paul Bonenfant
+ * @author Malkit Singh Date:2020/05/27
  */
 public class CardTrick {
 
+    /**
+     * This is the main method and program starts here
+     * @param args 
+     */
     public static void main(String[] args) {
         
+        int card_Value,temp;            //initializing variables
+        
+        String suit;
+        
+        char play='y';
+        
         Card[] magicHand = new Card[7];
-
-        for (int i = 0; i < magicHand.length; i++) {
-            Card c = new Card();
-            //c.setValue(insert call to random number generator here)
-            //c.setSuit(Card.SUITS[insert call to random number between 0-3 here])
+        
+        Scanner object=new Scanner(System.in);
+        while(play=='y' || play=='Y'){
+            temp=0;
+            System.out.println("Enter your card number(1-13): ");
+            card_Value=object.nextInt();                            //taking user input
+            System.out.println("Enter your suit(Hearts, Diamonds, Spades or Clubs): ");
+            suit=object.next();
+         
+            for (int i = 0; i < magicHand.length; i++) {                //loop to compare the user input with the magicHand array
+               Card c = new Card();
+               c.setValue((int)(1+Math.random()*13));
+               c.setSuit(Card.SUITS[(int)(Math.random()*4)]);
+            
+               if(card_Value==c.getValue() && suit.equalsIgnoreCase(c.getSuit())){
+                  System.out.println("You have got a magic hand");
+                  temp=1;
+                  break;
+            }
+                                  
+            }
+            if(temp==0)
+                System.out.println("You didn't get the magic hand");
+            
+            System.out.println("You want to play again?(Y or N)");
+            play=object.next().charAt(0);
         }
 
-        //insert code to ask the user for Card value and suit, create their card
-        // and search magicHand here
-        //Then report the result here
     }
 
 }
