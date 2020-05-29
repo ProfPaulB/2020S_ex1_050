a4feade2de86513b62b04fa0dffaada3e2c3491e
chirag1601 <chiragc@sheridancollege.ca>
2020-05-28 21:32:15 -0400

    Finalize the code and push some comment

--- src/pickacard/Card.java
+++ src/pickacard/Card.java
@@ -8,7 +8,7 @@ package pickacard;
  * This code is to be used in Exercise 1. When you create your own branch,
  * add your name as an author.
 
- * @author Hy Ngo
+ * @author Chirag Chirag 28 May 2020
  */
 public class Card {
 
