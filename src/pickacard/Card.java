432fb4ab85fa017272aedac9fb94efb1154a1e03
tappu <tappu@192.168.0.30>
2020-05-28 23:32:28 -0400

    First Commit

--- src/pickacard/Card.java
+++ src/pickacard/Card.java
@@ -6,9 +6,9 @@ package pickacard;
  * A suit (clubs, hearts, spades, diamonds).
  * There are 52 cards in a deck, no jokers.
  * This code is to be used in Exercise 1. When you create your own branch,
- * add your name as an author.
-
- * @author Hy Ngo
+ 
+ * @author Tapasvi Patel
+ * @date 28 May 2020
  */
 public class Card {
 
@@ -16,7 +16,8 @@ public class Card {
    private int value;//1-13
 
    public static final String [] SUITS = {"Hearts", "Diamonds", "Spades", "Clubs"};
-    /**
+    
+   /**
      * @return the suit
      */
     public String getSuit() {
@@ -26,8 +27,8 @@ public class Card {
     /**
      * @param suit the suit to set
      */
-    public void setSuit(String suit) {
-        this.suit = suit;
+    public void setSuit(int suit) {
+        this.suit = SUITS[suit];
     }
 
     /**
@@ -43,6 +44,4 @@ public class Card {
     public void setValue(int value) {
         this.value = value;
     }
-   
-
 }
--- src/pickacard/CardTrick.java
+++ src/pickacard/CardTrick.java
@@ -1,97 +1,61 @@
 package pickacard;
 
-import java.util.Random;
 import java.util.Scanner;
-import java.util.Vector;
-import static javax.management.Query.value;
-import static pickacard.Card.SUITS;
+
+
 /**
  * A class that fills a magic hand of 7 cards with random Card Objects and then asks the user to pick a card and
  * searches the array of cards for the match to the user's card. To be used as starting code in ICE 1
  *
- * 
- * @author Hy Ngo
+ * @author Tapasvi Patel
  */
 public class CardTrick {
 
     public static void main(String[] args) {
-           Scanner input = new Scanner(System.in);
+        
         Card[] magicHand = new Card[7];
-        String[] store = new String[7];
-        Random rdnumber = new Random();
-        
-        int number;
-        int Low = 1;
-        int High = 13;
-       
-        int luckn=0;
-        String suitn;
-        
-        Card luckyCard = new Card();
-        luckyCard.setValue(2);
-        luckyCard.setSuit("Diamonds");
-         luckn= luckyCard.getValue();
-         suitn = luckyCard.getSuit();
-        String luckynumber = luckn + suitn;
-        //this one is automatic find Lucky card 
-          for (int i = 0; i < magicHand.length; i++) {
-            Card c = new Card();
-                   
-             number = rdnumber.nextInt(13);
-           c.setValue(number);
-           c.getValue();
-           int d = rdnumber.nextInt(4);
-          c.setSuit(Card.SUITS[d]);
-          c.getSuit();
-          store[i] = c.getValue()+ c.getSuit();
-       if(  luckynumber.equals(store[i])){
-           System.out.println("correct");}
-       else
-          System.out.println("not correct");
-          }
-      
-          
-//  This is code for magic hand pick random card and searching
-       
-    /*  for (int i = 0; i < magicHand.length; i++) {
-            Card c = new Card();
-                
-          
-             number = rdnumber.nextInt(13);
-           c.setValue(number);
-           c.getValue();
-           int d = rdnumber.nextInt(4);
-          c.setSuit(Card.SUITS[d]);
-          c.getSuit();
-          store[i] = c.getValue()+ c.getSuit();
-    
+        Card LuckyCard= new Card();
+        LuckyCard.setValue(4);
+        LuckyCard.setSuit(2);
 
-      }
-       System.out.println("enter Card Number: ");
-        String cardnumber  = input.nextLine();
-        System.out.println("enter SUITS of Card: ");
-         String SUITS  = input.nextLine();
-        String results = cardnumber+SUITS;
+        for (int i = 0; i < magicHand.length; i++) {
+           // Card c = new Card();
+            magicHand[i]=new Card();           
+            magicHand[i].setValue((int) ((Math.random()*13)+1));//insert call to random number generator here)
+            magicHand[i].setSuit((int) (Math.round(Math.random()*3))); //0-3      
+            //System.out.println(magicHand[i].getSuit());
+            //System.out.println(magicHand[i].getValue());
+        } 
         
-        for (int u = 0; u < magicHand.length;u++){
-            String o =  store[u];
-                if(o.equals(results)){
-                             System.out.println("correctS" ); 
-
-                }
+       Scanner sc = new Scanner(System.in);
+        
+       System.out.print("Enter a Card value (1-13): ");
+        int val=sc.nextInt();
+        
+        System.out.print("Enter a Suit (Clubs, Spades, Diamonds, Hsearts): ");
+        String suit=sc.next();
+        
+        System.out.println("");
+        
+        int b=0;
+        
+        for (int i = 0; i < magicHand.length; i++) {
+            
+            if(magicHand[i].getValue()== val && (magicHand[i].getSuit().equals(suit)))
+            {
+                b++;
+            }                   
+                     
         }
-          System.out.println("magic hand is:");
-              for (int qu = 0; qu < magicHand.length;qu++){
-           System.out.println(store[qu] ); 
-        }*/
+       
         
-        //insert code to ask the user for Card value and suit, create their card
+        if(b>0) {
+            System.out.println("Hurray!! Your Guess is Correct.");
+        }
         
-        // and search magicHand here
-        
-        //Then report the result here
-    
+        else 
+            System.out.println("Sorry :( Try Again.");
+          
+    }
 
-
-}
 }
