e36fbfbe67a7b6b233127c7ed5fe40549bd8ff60
thonghy <ngothonghy@gmail.com>
2020-05-29 16:50:38 -0400

    Final Code

--- src/pickacard/CardTrick.java
+++ src/pickacard/CardTrick.java
@@ -25,9 +25,10 @@ public class CardTrick {
         Card luckyCard = new Card();
         luckyCard.setValue(2);
         luckyCard.setSuit("Diamonds");
-         luckn= luckyCard.getValue();
-         suitn = luckyCard.getSuit();
+        int luckn= luckyCard.getValue();
+         String suitn = luckyCard.getSuit();
         String luckynumber = luckn + suitn;
+        //create magic hand
          for (int i = 0; i < magicHand.length; i++) {
             Card c = new Card();
                    
@@ -38,14 +39,22 @@ public class CardTrick {
           c.setSuit(Card.SUITS[d]);
           c.getSuit();
           store[i] = c.getValue()+ c.getSuit();
+          int l = i+1;
+          //check lucky number
        if(  luckynumber.equals(store[i])){
            System.out.println("You got a Lucky card");}
        else
-          System.out.println("not correct");
+          System.out.println("Card"+ l +" not correct");
+      
+               
           }
-        
+         //show the magichand
+         System.out.println("the magicHand are :");
+                  for (int i = 0; i < magicHand.length; i++){
+                        System.out.println( store[i] );
+                  }
         //set value suits and get value suits  then store in array 
-        for (int i = 0; i < magicHand.length; i++) {
+ /*       for (int i = 0; i < magicHand.length; i++) {
             Card c = new Card();
              number = rdnumber.nextInt(12)+ 1;
             c.setValue(number);
@@ -69,7 +78,7 @@ public class CardTrick {
                     //report the resul
                              System.out.println("You Got MagicHand" ); 
         break;
-        }
+        } **/
        
           
             //c.setValue(insert call to random number generator here)
@@ -80,4 +89,4 @@ public class CardTrick {
     }
 
 }
-}
+
