51a773968a6aa6a62ba688308b5a06c5f6eb850d
Tappupatel14 <59375205+Tappupatel14@users.noreply.github.com>
2020-05-28 23:41:13 -0400

    Hardcoded:: LuckyCard object created

--- src/pickacard/CardTrick.java
+++ src/pickacard/CardTrick.java
@@ -1,13 +1,12 @@
 package pickacard;
 
-import java.util.Scanner;
-
 
 /**
  * A class that fills a magic hand of 7 cards with random Card Objects and then asks the user to pick a card and
  * searches the array of cards for the match to the user's card. To be used as starting code in ICE 1
  *
  * @author Tapasvi Patel
+ * @Date 28 May 2020
  */
 public class CardTrick {
 
@@ -15,41 +14,29 @@ public class CardTrick {
         
         Card[] magicHand = new Card[7];
         Card LuckyCard= new Card();
-        LuckyCard.setValue(4);
-        LuckyCard.setSuit(2);
+        LuckyCard.setValue(11);
+        LuckyCard.setSuit(3);
 
         for (int i = 0; i < magicHand.length; i++) {
-           // Card c = new Card();
+           
             magicHand[i]=new Card();           
-            magicHand[i].setValue((int) ((Math.random()*13)+1));//insert call to random number generator here)
-            magicHand[i].setSuit((int) (Math.round(Math.random()*3))); //0-3      
-            //System.out.println(magicHand[i].getSuit());
-            //System.out.println(magicHand[i].getValue());
-        } 
+            magicHand[i].setValue((int) ((1 + Math.random()*13)));
+            magicHand[i].setSuit((int) (Math.round(Math.random()*3)));      
+            
+        }
         
-       Scanner sc = new Scanner(System.in);
-        
-       System.out.print("Enter a Card value (1-13): ");
-        int val=sc.nextInt();
-        
-        System.out.print("Enter a Suit (Clubs, Spades, Diamonds, Hsearts): ");
-        String suit=sc.next();
-        
-        System.out.println("");
-        
-        int b=0;
+        int temp=0;
         
         for (int i = 0; i < magicHand.length; i++) {
             
-            if(magicHand[i].getValue()== val && (magicHand[i].getSuit().equals(suit)))
-            {
-                b++;
-            }                   
+            if(magicHand[i].getValue()== LuckyCard.getValue() && (magicHand[i].getSuit().equals(LuckyCard.getSuit()))) {
+                temp++;
+            }                                
                      
         }
        
         
-        if(b>0) {
+        if(temp>0) {
             System.out.println("Hurray!! Your Guess is Correct.");
         }
         
