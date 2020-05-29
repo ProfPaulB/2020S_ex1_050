4523b97e3fd25bd90327a456d0194957afc640e8
Abhinav Girdhar <girdhaab@sheridancollege.ca>
2020-05-28 20:08:21 -0400

    Made the instructed changes

--- src/pickacard/Card.java
+++ src/pickacard/Card.java
@@ -5,10 +5,11 @@ package pickacard;
  * a value (note that Ace = 1, Jack -11, Queen =12, King = 13)
  * A suit (clubs, hearts, spades, diamonds).
  * There are 52 cards in a deck, no jokers.
- * This code is to be used in Exercise 1. When you create your own branch,
- * add your name as an author.
+ * 
  * @author dancye
  * @author Paul Bonenfant
+ * @author Abhinav Girdhar
+ *  Date Modified - May 28, 2020.
  */
 public class Card {
 
