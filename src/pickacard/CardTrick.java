ecd5cfac1619f479d3dc3d83ea5c6f1f9025a734
Renome14 <pate2569@sheridancollege.ca>
2020-05-29 23:46:05 -0400

    Fixed the code. Commit 1.1

--- src/pickacard/CardTrick.java
+++ src/pickacard/CardTrick.java
@@ -4,7 +4,6 @@ package pickacard;
  * A class that fills a magic hand of 7 cards with random Card Objects and then asks the user to pick a card and
  * searches the array of cards for the match to the user's card. To be used as starting code in ICE 1
  *
- * @author dancye
  * @author Yash Patel 28th May
  */
 import java.util.Scanner;
@@ -13,9 +12,9 @@ public class CardTrick {
     public static void main(String[] args) {
         
         Card[] magicHand = new Card[7];
-        Card LuckyCard = new Card();
-        LuckyCard.setValue(1);
-        LuckyCard.setSuit("Hearts");
+        //Card LuckyCard = new Card();
+        //LuckyCard.setValue(1);
+       // LuckyCard.setSuit("Hearts");
         int value;
         String suit;
         String output = "";
@@ -30,6 +29,7 @@ public class CardTrick {
             System.out.println("Value = " +magicHand[i].getValue() + "Suit = "+magicHand[i].getSuit() );
             /*;Addded this line cause can't figure out if my code is right or worng 
               and it turns out to be wrong and cant really figure out why.
+            UPDATE_ figured out the mistake and fixed.
             */
         }
      //insert code to ask the user for Card value and suit, create their card   
@@ -41,8 +41,9 @@ public class CardTrick {
         
         
         // and search magicHand here
-        for (Card magicHand1 : magicHand) {
-            if (magicHand1.getValue() != value || !magicHand1.getSuit().equalsIgnoreCase(suit)) {
+        for (int i = 0; i < magicHand.length; i++) {
+        if(magicHand[i].getValue()==value && magicHand[i].getSuit().equals(suit))
+             {
                 output = "Bettter Luck Next Time";
             } else {
                 output ="Congrats It Matched";
