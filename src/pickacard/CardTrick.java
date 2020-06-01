a233290b936ec03e02f5a60832aeaca61ec94555
ayush <ayush@192.168.0.11>
2020-06-01 12:51:33 -0400

    Does not print all magic cards. Just print the one hard coded card.

--- src/pickacard/CardTrick.java
+++ src/pickacard/CardTrick.java
@@ -27,17 +27,20 @@ public class CardTrick {
             magicHand[i] = c;
         }
         //insert code to ask the user for Card value and suit, create their card
-        System.out.println("Enter the card number.(1-13)");
+        /*System.out.println("Enter the card number.(1-13)");
         int cardNum = scn.nextInt();
         System.out.println("Enter the card suit number. \nHearts \t         0 \nDiamonds \t  1 \nSpades \t         2 \nClubs \t         3");
         
-        int cardSuit = scn.nextInt();
-        Card userChoice = new Card(Card.SUITS[cardSuit],cardNum);
+       int cardSuit = scn.nextInt();*/
+        int cardNum = 4;
+        int cardSuit =0;
+       Card userChoice = new Card(Card.SUITS[cardSuit],cardNum);
         // and search magicHand hereCard 
         System.out.print("Your Card:" + userChoice.getValue() + " ");
             System.out.println(userChoice.getSuit());
           /**  System.out.println("Magic Hand: ");*/
         for (int i = 0; i < magicHand.length; i++) {
+            
           /**  
             System.out.print(magicHand[i].getValue() + " ");
             System.out.println(magicHand[i].getSuit());*/
